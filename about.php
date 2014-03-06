d3a2b65aee07926c253e7d3a2c760bb039ca9f42
Alexander Lingris <hi@lexx.gr>
2014-03-06 21:41:23 +0200

    init

---
+++ about.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Soho</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="./assets/css/style.css">
+    
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+    <div class="header">
+        <div class="logo">
+            <a href="./">YOURLOGO</a>
+        </div>
+    </div>
+    
+    <div class="content row">
+        <div class="col-xs-3 col-sm-3 col-md-2 col">
+            &nbsp;
+            <hr>
+            <ul class="menu">
+                <li><a href="./">Home</a></li>
+                <li><a class="active" href="./about.php">About</a></li>
+                <li><a href="./archive.php">Archive</a></li>
+            </ul>
+            
+            <hr>
+            
+            <ul class="menu">
+                <li><a href="#">Behance</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Linkedin</a></li>
+            </ul>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-8 col">
+            &nbsp;
+            <hr>
+            <div class="about-text">
+                <p>Picking up after issue #3’s unsettling revelation of Sissy’s birth, Pretty Deadly #4 immediately kicks into gear as the rogues and not-quite-heroes of this haunting western tale unite to save Sissy. Writer Kelly Sue DeConnick delivers another air-tight script with this issue, brought to the page by artist Emily Rios and her beautiful sense of energy and space. Jordie Bellaire returns to round out this impressive creative team with her subtle color palettes, lending depth and softness to Rios’ otherworldly locales and delicate, fine-featured characters.</p>
+                <p>Opening the issue with Bones Bunny and Butterfly, Johnny and Molly arrive at the river’s edge to pull Sissy from the water. In a scene that continues to play with the idea of animal forms, switching back and forth between Molly and Johnny’s respective namesakes, he explains (in part) the story behind Death’s binder. As they set off, we see Alice return to the underworld in her butterfly form, finding Death none too pleased with her. She promises to ally herself with Ginny to deliver Sissy, the Ascendant, to him, and he warns her not to disappoint him again as he restores her body. Alone again, Ginny’s mother, Fox’s wife and Death’s prisoner in the underworld, appeals to him for her release. Too in love to let her go, he promises he will free the both of them instead once he has Sissy.</p>
+            </div>
+        </div>
+        <div class="col-xs-3 col-sm-3 col-md-2 col">
+            Categories
+            <hr>
+            <ul class="menu">
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Test</a></li>
+                <li><a href="#">Test</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    <div class="footer">
+        &copy; Powered by <a href="http://www.writus.com">Writus</a>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script src="./assets/js/imgLiquid-min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    
+    <script>
+        $(document).ready(function() {
+            $(".article-image").imgLiquid();
+        
+        });
+    </script>
+
+  </body>
+</html>
